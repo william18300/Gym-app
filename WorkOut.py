6ff02bd823bad42e2bb1041d5e0fc321f997b515
william18300 <159724470+william18300@users.noreply.github.com>
2026-04-22 23:40:50 -0400

    made a class and added env and venv

---
+++ WorkOut.py
@@ -0,0 +1,9 @@
+class WorkOut:
+
+    def __init__(self, workout, sets, reps, weight):
+        self.workout  = workout
+        self.sets = sets
+        self.reps = reps 
+        self.weight = weight
+
+    
